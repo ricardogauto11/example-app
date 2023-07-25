f0c6e4ba1c0882a098d951b729e0f887866a6f11
Ricardo Gauto <ricardo.gauto@redtecnologica.org>
2023-07-25 18:35:42 -0300

    Devolviendo Posts

--- app/Http/Controllers/API/v2/PostController.php
+++ app/Http/Controllers/API/v2/PostController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\API\v2;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\v2\PostCollection;
+use App\Http\Resources\v2\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -15,7 +17,9 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::latest()->paginate();
+
+        return new PostCollection($posts);
     }
 
     /**
@@ -37,7 +41,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        return new PostResource($post);
     }
 
     /**
