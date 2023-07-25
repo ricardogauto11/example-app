e3e9eac102347b257a54532f4acbb5eb66bb332e
Ricardo Gauto <ricardo.gauto@redtecnologica.org>
2023-07-25 18:44:56 -0300

    Restringiendo acceso a rutas

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,9 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::apiResource('v1/posts', PostV1::class) 
-    ->only(['index', 'show', 'destroy']);
+    ->only(['index', 'show', 'destroy'])
+    ->middleware('auth:sanctum');
 
 Route::apiResource('v2/posts', PostV2::class)
-    ->only(['index', 'show', 'destroy']);
+    ->only(['index', 'show']) 
+    ->middleware('auth:sanctum');
